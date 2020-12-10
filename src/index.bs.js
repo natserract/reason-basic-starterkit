3f489a85273e13c643c476ea49baf1ef9c6a02f7
natserract <benjaminstwo@gmail.com>
2020-12-10 11:13:28 +0800

    Modified package.json scripts and add another packages

--- src/index.bs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var React = require("react");
-var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
-var App$ReasonReactBasicStarterkit = require("./app/app.bs.js");
-
-ReactDOMRe.renderToElementWithId(React.createElement(App$ReasonReactBasicStarterkit.make, { }), "__reason");
-
-/*  Not a pure module */
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.bs.js',
@@ -6,10 +7,21 @@ module.exports = {
   // to 'development' as per webpack documentation. Again, you don't have to
   // use webpack or any other bundler during development! Recheck README if
   // you didn't know this
-  mode: 'production',
+  mode: 'development',
   devtool: 'inline-source-map',
   output: {
     path: path.join(__dirname, "build"),
     filename: 'index.js',
   },
+  devServer: {
+    publicPath: "/",
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "./index.html"
+    }),
+  ]
 };
